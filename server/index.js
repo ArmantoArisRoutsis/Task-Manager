00ee32f5846ccfa4a29bbbf093794aba77ffb0ee
Routsis Armanto <ar.routsis@gmail.com>
2021-01-29 07:43:33 +0200

    Minnor Fix

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(cors());
 
 app.use("/tasks", taskRoutes)
 
-const CONNECTION_URL = "mongodb+srv://admin-armanto:6942096892@taskmanager.4lrbg.mongodb.net/taskManagerDB?retryWrites=true&w=majority"
+const CONNECTION_URL = MONGO_URI
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL,{useNewUrlParser:true, useUnifiedTopology:true})
